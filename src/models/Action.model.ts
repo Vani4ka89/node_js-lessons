37cb1d4ab163af26b1afbbdc4d0edc52e069a32d
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-06-23 21:36:48 +0300

    forgot password

--- src/models/Action.model.ts
+++ src/models/Action.model.ts
@@ -1,6 +1,6 @@
 import { model, Schema, Types } from "mongoose";
 
-import { EActionTokenTypes } from "../enums/action-token-type";
+import { EActionTokenTypes } from "../enums";
 import { User } from "./User.mode";
 
 const actionSchema = new Schema({
