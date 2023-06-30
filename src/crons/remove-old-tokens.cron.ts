1f50907d8a888b36aadb986494ea47ed2fa5ef87
Vani4ka89 <ivan.tym4ak@gmail.com>
2023-06-30 19:36:19 +0300

    added cron

---
+++ src/crons/remove-old-tokens.cron.ts
@@ -0,0 +1,16 @@
+import { CronJob } from "cron";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+
+import { Token } from "../models";
+
+dayjs.extend(utc);
+
+const tokensRemover = async () => {
+  const previousMonth = dayjs().utc().subtract(30, "days");
+  await Token.deleteMany({
+    createdAt: { $lte: previousMonth },
+  });
+};
+
+export const removeOldTokens = new CronJob("* * * * * *", tokensRemover);
